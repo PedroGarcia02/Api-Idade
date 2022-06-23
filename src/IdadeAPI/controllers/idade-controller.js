8c1726aa1c29011caef524ec1a2f44bf13986670
PedroJacks <47866334+PedroGarcia02@users.noreply.github.com>
2022-06-23 17:24:30 -0300

    Add files via upload

--- src/IdadeAPI/controllers/idade-controller.js
+++ src/IdadeAPI/controllers/idade-controller.js
@@ -1,29 +1,45 @@
 const axios = require('axios');
 
-function testenome(str) {
-    return /^[a-zA-Z]+$/.test(str);
-  }
 
 class IdadeController {
     async buscaIdade (req, res) {
+    const { nome }  = req.body;
+    const { nome2 } = req.body;
+    const { nome3 } = req.body;
+    
+   let nomes = [nome.toLowerCase(), nome2.toLowerCase(), nome3.toLowerCase()]
+   let contador = 0
+nomes.forEach(nome => { 
+   let Primeironome = nome.split(' ')[0] // pega só a primeira palavra
+   let  nomenew = Primeironome.normalize("NFD").replace(/[\u0300-\u036f]/g, "") // tira acento
+    nomes[contador] = nomenew  
+    contador++
+});
+    
+    let final = []
+    let contador2 = 0
+while (contador2 < 3){ 
+    final[contador2] = await buscaIdadeNaAPI(nomes[contador2]);
+    contador2++
+ };
 
-    const { nome } = req.body;
 
-    if(testenome(nome)) {
+ let peoples = sortByKey(final, "age");
 
-    const info = await buscaIdadeNaAPI(nome);
-
-    return res.render('ShowIdade', {info});
-
-    }else{
-        res.send("Digite um nome com apenas letras")
+ function sortByKey(array, key) {
+    return array.sort(function(a, b) {
+        var x = a[key]; var y = b[key];
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
     }
+
+    return res.render('ShowIdade', {peoples});
 }
 }
 
 const buscaIdadeNaAPI = async (nome) => {
 
-   const URL = `https://api.agify.io/?name=${nome}`;
+   const URL = "https://api.agify.io/?name="+nome;
     try {
         const resposta = await axios.get(URL);
        return resposta.data;
--- src/index.js
+++ src/index.js
@@ -5,14 +5,13 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', './src/IdadeAPI/views');
-
 app.use(express.static('public'));
-
 app.use(express.urlencoded({
     extended: true
 }));
 
 app.use(express.json());
+
 const IdadeRouter = require('./IdadeAPI/routes/idade-routes');
 
 app.use('/', IdadeRouter);
